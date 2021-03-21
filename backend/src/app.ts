d97543c1f253baae31b79c2a0a10f7cb448f4c32
Andrew <andrew@conducttr.com>
2021-03-21 02:41:58 +0000

    adding changes to index.html, changing description / title

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import "express-async-errors";
-import { json } from "body-parser";
 import cookieSession from "cookie-session";
 import { currentUserRouter } from "./routes/current-user";
 import { signInRouter } from "./routes/signin";
